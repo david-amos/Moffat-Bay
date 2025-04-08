93faf47a9f3fe601a57cd0e9c8a7c5e824dbdbd1
david-amos <davidamos2356@gmail.com>
2025-04-07 19:12:49 -0500

    Initial File Structure

---
+++ src/main/java/beans/User.java
@@ -0,0 +1,64 @@
+package beans;
+
+import java.util.Date;
+
+public class User {
+	private String UserEmail;
+	private String FirstName;
+	private String LastName;
+	private String PhoneNumber;
+	private Date birthDate;
+	private String Password;
+	public String getUserEmail() {
+		return UserEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		UserEmail = userEmail;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return PhoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		PhoneNumber = phoneNumber;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public User(String userEmail, String firstName, String lastName, String phoneNumber, Date birthDate,
+			String password) {
+		super();
+		UserEmail = userEmail;
+		FirstName = firstName;
+		LastName = lastName;
+		PhoneNumber = phoneNumber;
+		this.birthDate = birthDate;
+		Password = password;
+	}
+	public User() {
+		super();
+	}
+	
+	
+
+}
